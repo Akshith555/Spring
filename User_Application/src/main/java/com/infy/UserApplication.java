e8f8a707257b6ff80179c05ae9d2f46af619462e
Akshith555 <akshithgoudranger@gmail.com>
2021-04-14 15:10:58 +0530

    Some changes

--- User_Application/src/main/java/com/infy/UserApplication.java
+++ User_Application/src/main/java/com/infy/UserApplication.java
@@ -9,6 +9,7 @@ public class UserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 		System.out.println("Hello World");
+		System.out.println("Hello ");
 	}
 
 }
